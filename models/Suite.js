a7df3e2550566e5dbc96cb866c4c7280adcb0f26
Jaaystones <Joelpaul345@gmail.com>
2023-09-05 23:34:33 +0100

    :wrench: Database Models

---
+++ models/Suite.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const SuiteSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    maxPeople: {
+      type: Number,
+      required: true,
+    },
+    desc: {
+      type: String,
+      required: true,
+    },
+    suiteNumbers: [{ number: Number, unavailableDates: { type: [Date] } }],
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("Suite", SuiteSchema);
