efb50d001ad59f2c4365f43e978a13395e88d343
Jaaystones <Joelpaul345@gmail.com>
2023-09-08 16:00:32 +0100

    test later

--- routes/users.js
+++ routes/users.js
@@ -28,7 +28,7 @@ router.put("/:id", verifyUser, updateUser);
 router.delete("/:id", verifyUser, deleteUser);
 
 //GET
-router.get("/:id", verifyUser, getUser);
+router.get("/:id", verifyUser, verifyToken, getUser);
 
 //GET ALL
 router.get("/", verifyAdmin, getUsers);
--- utility/tokenVerification.js
+++ utility/tokenVerification.js
@@ -1,5 +1,5 @@
 import jwt from "jsonwebtoken";
-import  { createError } from "../utility/error.js";
+import { createError } from "../utility/error.js";
 
 export const verifyToken = (req, res, next) => {
   const token = req.cookies.access_token;
@@ -15,11 +15,13 @@ export const verifyToken = (req, res, next) => {
 };
 
 export const verifyUser = (req, res, next) => {
-  if (req.user.id === req.params.id || req.user.isAdmin) {
-    next();
-  } else {
-    return next(createError(403, "You are not authorized!"));
-  }
+  verifyToken(req, res, next, () => {
+    if (req.user.id === req.params.id || req.user.isAdmin) {
+      next();
+    } else {
+      return next(createError(403, "You are not authorized!"));
+    }
+  });
 };
 
 export const verifyAdmin = (req, res, next) => {
